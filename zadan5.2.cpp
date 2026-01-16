3bf2d2c52c0b55e125cbebd6e6e551e20349ded4
dimonkrutoiprogrammist <dmitry2008cita@gmail.com>
2026-01-16 16:32:31 +0900

    Update zadan5.2.cpp

--- zadan5.2.cpp
+++ zadan5.2.cpp
@@ -1,29 +1,30 @@
 #include <iostream>
 using namespace std;
 
-bool SimpleNumber(int a){           //Функция проверки на простое число: возвращает true или false
+bool SimpleNumber(int a){           
     for(int i = 2; i * i <= a; i++){
         if(a % i == 0){
-            return false;           //Условие: если число делится на четные или нечетные, то false
+            return false;           
         }
     }
-    return true;                    //Иначе true
+    return true;                   
 }
 
 int main(){
     setlocale(LC_ALL, "ru");
 
-    int a; //Переменная
+    int a; 
 
     cout << "a: ";
-    cin >> a;       //Ввод числа
+    cin >> a;       
 
-    if(SimpleNumber(a)){        //Условие: если функция возвращает true, то число четное
+    if(SimpleNumber(a)){        
         cout << "Simple";
     }
-    else{                       //Иначе функция возвращает false, число нечетное
+    else{                       
         cout << "Not Simple";
     }
 
     return 0;
+
 }
